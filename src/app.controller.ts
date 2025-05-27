6ace980a9b843f6d2c98c6413b1072cc2db4542b
EmitoStuer <stuer.87@gmail.com>
2025-05-26 23:38:18 -0300

    segunda Clase

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { AppService, iTrack } from './app.service';
+
+//ENcargado de Ejecutar la Logica
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('tracks')
+  getTrack(): iTrack[] {
+    return this.appService.getTracks();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,43 @@
 import { Injectable } from '@nestjs/common';
+//Logica de Negocio de La aplicacion.
+
+export interface iTrack{
+  id: number,
+  title: string,
+  duration: number,
+  artist: string
+}
+
+export const tracks: iTrack[] = [
+  {
+    id:1,
+    title: "cancion 1",
+    duration: 120,
+    artist:"interprete 1"
+  },
+  {
+    id:2,
+    title: "cancion 2",
+    duration: 130,
+    artist:"interprete 2"
+  },
+  {
+    id:3,
+    title: "cancion 3",
+    duration: 140,
+    artist:"interprete 3"
+  },
+  {
+    id:4,
+    title: "cancion 4",
+    duration: 150,
+    artist:"interprete 4"
+  }
+]
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getTracks(): iTrack[] {
+    return tracks;
   }
 }
